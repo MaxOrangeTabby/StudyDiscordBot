2e8e88b7145d0a131ba218ea9a79a520b6b38dcd
ProudOne7 <peternyen7@gmail.com>
2024-06-26 12:12:54 -0500

    For General Bot Responses

---
+++ cogs/responses.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Responses(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='command_help')
+    async def help(self, context):
+        return
+    
+    
+async def setup(bot):
+    await bot.add_cog(Responses(bot))
