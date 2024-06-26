27a083e5dab8c63d0bad488cf0c2bdb965e9b160
ProudOne7 <peternyen7@gmail.com>
2024-06-26 13:24:15 -0500

    Added Help Command

--- cogs/responses.py
+++ cogs/responses.py
@@ -7,7 +7,13 @@ class Responses(commands.Cog):
 
     @commands.command(name='command_help')
     async def help(self, context):
-        return
+        try: 
+            await context.send(f"Here are my commands:\n" 
+                            "**add_flashcard_set** - Create a new flashcard set or add some to an existing set\n" 
+                            "**view_flashcard_sets** - View all of the flashcard sets created\n "
+                            "**view_flashcard_set** - View the flashcards in a certain set")
+        except Exception as err:
+            await context.send(f"Error Occurred: {err}")
     
 async def setup(bot):
     await bot.add_cog(Responses(bot))
