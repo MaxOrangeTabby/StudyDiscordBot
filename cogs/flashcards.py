e32c6e30e48a85bee5bd4f748adcceed891e233e
ProudOne7 <peternyen7@gmail.com>
2024-06-28 21:41:53 -0500

    Moved Flashcard Sets to Base

--- cogs/flashcards.py
+++ cogs/flashcards.py
@@ -4,13 +4,9 @@ from discord.ext import commands
 
 class Flashcards(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot;
-        self.flashcards = []
+        self.bot = bot
 
-        self.flashcard_sets = {}
-        self.flashcard_sets_amt = 0
-
-    def check(context, other):
+    def check(self,context, other):
         return other.author == context.author and other.channel == context.channel
     
     # Used to add new flashcards to a set or edit an existing one
@@ -22,8 +18,9 @@ class Flashcards(commands.Cog):
         try: 
             await context.send("Would you like to add a new set or edit an existing one?\nType \"NEW\" or \"EDIT\"")  
             set_option = await self.bot.wait_for('message', check=check_other, timeout=30.0)
+            str_option_str = (set_option.content).lower()
 
-            if(set_option.content != "NEW" and set_option.content != "EDIT"):
+            if(str_option_str != "new" and str_option_str != "edit"):
                 await context.send("Not a valid option! :(")
                 return
 
@@ -35,18 +32,18 @@ class Flashcards(commands.Cog):
             for index in range(amt_flashcards):
                 await self.add_flashcard(context, new_flashcards)
             
-            if(set_option.content == "NEW"):
+            if(str_option_str == "new"):
                 await context.send("What would you like to call this new set?")
                 set_name = await self.bot.wait_for('message', check=check_other, timeout=30.0)
                 
-                self.flashcard_sets[set_name.content] = new_flashcards
+                self.bot.flashcard_sets[set_name.content] = new_flashcards
                 await context.send("Flashcard set created!")
-                self.flashcard_sets_amt += 1
+                self.bot.flashcard_sets_amt += 1
             else:
                 await context.send("Which set would you like to add this to?")
                 set_name = await self.bot.wait_for('message', check=check_other, timeout=30.0)
 
-                self.flashcard_sets[set_name.content] = new_flashcards
+                self.bot.flashcard_sets[set_name.content] = new_flashcards
                 await context.send("New Flashcards added!")
 
         except Exception as err:
@@ -59,17 +56,17 @@ class Flashcards(commands.Cog):
             return self.check(context, other)
         
         try:
-            for key, flashcard_set in self.flashcard_sets.items():
+            for key, flashcard_set in self.bot.flashcard_sets.items():
                 embedCard = discord.Embed(title = f"Set: {key}")
                 await context.send(embed=embedCard)
             await context.send("Would you like to view one of the sets?\n Type \"YES\" or \"NO\"")
             user_res = await self.bot.wait_for('message', check=check_other, timeout=30.0)
 
-            if((user_res.content).lower() != "yes" or (user_res.content).lower() != "no"):
+            if((user_res.content).lower() != "yes" and (user_res.content).lower() != "no"):
                 await context.send("Now a valid option! :(")
                 return
             
-            self.view_flashcard_set(context)
+            await   self.view_flashcard_set(context)
             
         except Exception as err:
             await context.send(f"Error Occurred: {err}")
@@ -90,8 +87,10 @@ class Flashcards(commands.Cog):
                     await context.send("Exiting process!")
                     return
 
-                if(set_name.content in self.flashcard_sets):
-                    self.view_all_flashcard(context, self.flashcard_sets[set_name.content])
+                if(set_name.content in self.bot.flashcard_sets):
+                    await context.send(f"All flashcards in Set {set_name.content}:")
+                    await self.view_all_flashcard(context, self.bot.flashcard_sets[set_name.content])
+                    return
                 else:
                     await context.send("That set doesn't exist, try again or type \"Cancel\" to exit")
         except Exception as err:
@@ -109,7 +108,7 @@ class Flashcards(commands.Cog):
             await context.send("Enter Flashcard Body")
             body = await self.bot.wait_for('message', check=check_other, timeout=30.0)
 
-            list_flashcard.append((title.content, body.content))
+            list_flashcard.append({title.content, body.content})
             await context.send("New Flashcard Added!")
 
         except Exception as err:
@@ -117,10 +116,7 @@ class Flashcards(commands.Cog):
 
 
     # Helper function to view all of the flashcards in a set
-    async def view_all_flashcard(self, context, flashcard_set):
-        def check_other(other):
-            return self.check(context, other)
-        
+    async def view_all_flashcard(self, context, flashcard_set):        
         for title, body in flashcard_set:
             embedCard = discord.Embed(title=title, description = body)
             await context.send(embed=embedCard)
